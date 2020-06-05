335436c14f3ae28d75067ea00f216c88df76c14b
Uche Nweke Jr <unwekejr@gmail.com>
2020-06-05 01:09:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 def batch_badge_creator(array)
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   speakers.each do |name|
-  
+  return "Hello, my name is #{name}."
   
   
 end
